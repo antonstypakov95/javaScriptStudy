0a8ee99b478093e1f64a0caab78d101cf9bcae22
Anton <stupakovanton@gmail.com>
2023-07-27 21:42:44 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+'use strict'; /* новый строгий режим */
+
+// alert('Hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18', '');
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('какая ваша фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+// console.log(typeof(answers)); 
+
+// const category = 'toys';
+// console.log(`htttps://someurl.com/${category}/5/${category}`);
+
+const userName = 'Anton';
+alert(`Привет, ${userName}`);
